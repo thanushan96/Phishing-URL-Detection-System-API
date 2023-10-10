c3545afeef0a792d189a305ad0b0cd6819c1305c
thanushan96 <ravishankarthanushan96@gmail.com>
2023-10-10 11:53:08 +0530

    feature extraction function call under app.py

--- NLP based detection/app.py
+++ NLP based detection/app.py
@@ -28,52 +28,52 @@ import certificate_info
 #%%
 
 
-# df = pd.read_csv('NLP_Dataset.csv')
-# x= df['url']
-# y = df['label']
+df = pd.read_csv('NLP_Dataset.csv')
+x= df['url']
+y = df['label']
 
-# voc_size = 10000
-# messages = x.copy()
+voc_size = 10000
+messages = x.copy()
 
-# corpus = []
-# for i in range(0, len(messages)):
-#     review = re.sub('[^a-zA-Z]',' ',urlparse(messages[i]).netloc)
-#     review = review.lower()
-#     review = review.split()
-#     review=' '.join(review)
-#     corpus.append(review)
+corpus = []
+for i in range(0, len(messages)):
+    review = re.sub('[^a-zA-Z]',' ',urlparse(messages[i]).netloc)
+    review = review.lower()
+    review = review.split()
+    review=' '.join(review)
+    corpus.append(review)
 
-# onehot_repr=[one_hot(words,voc_size)for words in corpus]
-# sent_length = 50
-# embedded_docs= pad_sequences(onehot_repr,padding='pre',maxlen=sent_length)
+onehot_repr=[one_hot(words,voc_size)for words in corpus]
+sent_length = 50
+embedded_docs= pad_sequences(onehot_repr,padding='pre',maxlen=sent_length)
 
-# embedded_docs = np.array(embedded_docs)
+embedded_docs = np.array(embedded_docs)
 
-# #x_final = np.array(embedded_docs)
-# x_final = embedded_docs
-# y_final  = np.array(y)
-# from sklearn.model_selection import train_test_split
-# x_train,x_test,y_train,y_test = train_test_split(x_final,y_final,test_size=0.20,random_state=15)
-
-
-# #make the model and train it
-# embedding_vector_features=100
-# model = Sequential()
-# model.add(Embedding(voc_size,embedding_vector_features,input_length=sent_length))
-# model.add(LSTM(100))
-# model.add(Dense(1,activation='sigmoid'))
-# model.compile(loss='binary_crossentropy',optimizer='adam',metrics=['accuracy'])
+#x_final = np.array(embedded_docs)
+x_final = embedded_docs
+y_final  = np.array(y)
+from sklearn.model_selection import train_test_split
+x_train,x_test,y_train,y_test = train_test_split(x_final,y_final,test_size=0.20,random_state=15)
 
 
-# model.fit(x_train,y_train,validation_data=(x_test,y_test),epochs=2,batch_size=64)
+#make the model and train it
+embedding_vector_features=100
+model = Sequential()
+model.add(Embedding(voc_size,embedding_vector_features,input_length=sent_length))
+model.add(LSTM(100))
+model.add(Dense(1,activation='sigmoid'))
+model.compile(loss='binary_crossentropy',optimizer='adam',metrics=['accuracy'])
 
-# y_pred1=model.predict(x_test) 
-# classes_y1=np.round(y_pred1).astype(int)
-# from sklearn.metrics import confusion_matrix
-# confusion_n = confusion_matrix(y_test,classes_y1)
-# from sklearn.metrics import accuracy_score
-# print(accuracy_score(y_test, classes_y1))
-# model.save("model_NLP.h5")
+
+model.fit(x_train,y_train,validation_data=(x_test,y_test),epochs=2,batch_size=64)
+
+y_pred1=model.predict(x_test) 
+classes_y1=np.round(y_pred1).astype(int)
+from sklearn.metrics import confusion_matrix
+confusion_n = confusion_matrix(y_test,classes_y1)
+from sklearn.metrics import accuracy_score
+print(accuracy_score(y_test, classes_y1))
+model.save("model_NLP.h5")
 #%%
 from flask import Flask
 from flask_cors import CORS
@@ -142,10 +142,10 @@ def login():
             return jsonify(message='Login successful!')
         else:
             return jsonify(message='Login failed. Please check your credentials and try again.'), 401
-
+        
 @app.route('/logout', methods=['POST'])
 def logout():
-    # You can handle the logout logic here if needed
+    
     return jsonify(message='Logout successful!')
 
 @app.route('/users', methods=['GET'])
@@ -177,6 +177,9 @@ def predict():
     for i in request.form.values():
         messages = i
         
+        # Call the extract_features function 
+        extract_features(messages)  
+        
         # Check if the URL is in valid format
         messages = urlparse(messages).netloc
         if messages == '':
@@ -185,6 +188,9 @@ def predict():
         else:
             status = "URL is in valid format:"
             a = 2
+            
+        
+
         
         # Reading the whitelist
         f = open("whitelist.txt", "r")
@@ -252,4 +258,4 @@ def predict():
         return jsonify(response_data)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8000, debug=True)
+    app.run(host='0.0.0.0', port=8000, debug=False)
